74dfb7abf986ba4019f49fe7259e4960ce7fc958
dragon-fish <dragon-fish@qq.com>
2026-01-19 22:23:04 +0800

    fix: layout fix

--- frontend/components/Layout/LayoutDefault.vue
+++ frontend/components/Layout/LayoutDefault.vue
@@ -1,24 +1,18 @@
 <template lang="pug">
-NLayout.full-layout-container.default-layout(content-class='min-h-100vh flex flex-col', native-scrollbar)
-  NLayoutContent.main-content-container(flex-1, native-scrollbar)
+NLayout.full-layout-container.default-layout(
+  content-class='min-h-100vh flex flex-col',
+  native-scrollbar,
+  position='absolute'
+)
+  GlobalHeader(fixed, left-0, top-0, w-full, z-10)
+  NLayoutContent.main-content-container(flex-1, native-scrollbar, style='padding-top: 60px')
     slot
-  GlobalHeader(fixed, top-0, left-0, right-0, z-10)
   GlobalFooter
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped lang="sass">
-#full-app-container
-  min-height: 100vh
-  display: flex
-  flex-direction: column
-
-.main-content-container
-  flex: 1
-  padding-top: 60px
-  padding-bottom: 60px
-
 :deep(.root-router-view)
   width: 1200px
   max-width: calc(100vw - 2rem)
